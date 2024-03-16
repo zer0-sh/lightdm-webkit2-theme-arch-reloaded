67257b3dfc19d2285821bde4c56df9d08d29c479
zer0-sh <johan.steven.munoz@correounivalle.edu.co>
2024-03-16 15:53:45 -0500

    Update license and remove unused files

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+var selectedSessionKey;
+
+window.onload = function() {
+    getImg();
+    input.focus();
+    input.select();
+    input.value = lightdm.select_user_hint;
+    if(input.value) {
+        authenticate(input.value);
+    }
+
+    // get the sessions installed
+    var sessions = lightdm.sessions;
+    var sessionOptions = document.getElementById("session-options");
+    var sessionSelectedMessage = document.getElementById("session-selected-message");
+    sessionOptions.innerHTML = "";
+
+    // Add each session
+    sessions.forEach(function(session) {
+        var option = document.createElement("a");
+        option.href = "#";
+        option.textContent = session.name;
+        option.addEventListener("click", function() {
+            sessionOptions.querySelectorAll('a').forEach(function(opt) {
+                opt.classList.remove("selected");
+            });
+            this.classList.add("selected");
+            sessionSelectedMessage.textContent = "Session changed to " + session.name + ":P";
+            // Almacena la clave de sesión seleccionada en la variable global
+            selectedSessionKey = session.key;
+        });
+        sessionOptions.appendChild(option);
+    });
+}
+
 var input = document.getElementById("input");
 input.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {
@@ -9,15 +44,24 @@ window.authentication_complete = function() {
     if (lightdm.is_authenticated) {
         console.log("Authenticated!");
         $( 'body' ).fadeOut( 1000, () => {
-            lightdm.login(lightdm.authentication_user, null);
+            // Utiliza la sesión seleccionada después de la autenticación
+            lightdm.login(lightdm.authentication_user, selectedSessionKey);
         } );
+    } else {
+        getImg();
+        input.value = "";
+        input.placeholder = "user";
+        input.type = "text";
+        input.disabled = false;
+        input.focus();
+        input.select();
     }
 }
 
 function pad(a, b) {
     return (1e15 + a + "").slice(-b);
 }
-
+// Add a random wallpaper
 function getImg() {
     index = Math.floor(Math.random() * 25);
     console.log(pad(index,2));
@@ -25,27 +69,15 @@ function getImg() {
         "url(wallpapers/" + pad(index, 2) + ".png)";
 }
 
-window.onload = function() {
-    getImg();
-    input.focus();
-    input.select();
-}
-
 function authenticate(input_text) {
-    if(!lightdm.in_authentication) {
+    if(!lightdm.in_authentication || !lightdm.authentication_user) {
         lightdm.authenticate(input_text);
         input.value = "";
         input.type = "password";
         input.placeholder = "password";
-    } else if(!lightdm.authentication_user) {
-        lightdm.respond(input_text);
-        input.value = "";
-        input.type = "password";
-        input.placeholder = "password";
+        input.disabled = false;
     } else {
+        input.disabled = true;
         lightdm.respond(input_text);
-        input.value = "";
-        input.type = "text";
-        input.placeholder = "user";
     }
 }
